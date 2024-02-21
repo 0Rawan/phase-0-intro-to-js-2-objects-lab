c79a8e03327906965a51580d7a1030692ddda0c5
Rawan <r.k096@hotmail.com>
2024-02-21 22:05:01 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ function updateEmployeeWithKeyAndValue(employee, key, value){
     ...employee,
     [key]: value,}
 }
+
 function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
     employee[key] = value
     return employee
 }
+
 function  destructivelyDeleteFromEmployeeByKey(employee, key){
     delete employee[key]
     return employee
